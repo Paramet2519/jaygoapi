61cac3ecf11848a3e4b8b4ca1f02fed5ccbacd98
Paramet <paramet@jaymart.co.th>
2021-03-06 14:31:05 +0700

    Initialize GO Project

--- server.go
+++ server.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/labstack/echo/v4"
 )
@@ -14,6 +16,8 @@ func helloHandler(c echo.Context) error {
 func main() {
 	e := echo.New()
 	e.GET("/hello", helloHandler)
-	e.Start(":1234")
+	port := os.Getenv("PORT")
+	log.Panicln("port", port)
+	e.start(":" + port)
 
 }
