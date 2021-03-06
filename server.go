e18a161c2f1c9c15cff76c5470415829cb41866a
Paramet <paramet@jaymart.co.th>
2021-03-06 15:29:30 +0700

    Initialize GO Project

--- server.go
+++ server.go
@@ -9,6 +9,23 @@ import (
 	"github.com/labstack/echo/v4/middleware"
 )
 
+type Todo struct {
+	ID     int    `json:"id"`
+	Title  string `json:"title"`
+	Status string `json:"status"`
+}
+
+var todos = map[int]*Todo{
+	1: &Todo{ID: 1, Title: "pay phone bills", Status: "active"},
+}
+
+func getTodosHandler(c echo.Context) error {
+	items := []*Todo{}
+	for _, item := range todos {
+		items = append(items, item)
+	}
+	return c.JSON(http.StatusOK, items)
+}
 func helloHandler(c echo.Context) error {
 	return c.JSON(http.StatusOK, map[string]string{
 		"message": "Paramet",
@@ -20,6 +37,7 @@ func main() {
 	e.Use(middleware.Recover())
 
 	e.GET("/hello", helloHandler)
+	e.GET("Todos", getTodosHandler)
 	port := os.Getenv("PORT")
 	log.Println("port", port)
 	e.Start(":" + port)
